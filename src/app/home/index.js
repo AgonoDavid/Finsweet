7041fcc463871af3423fca6a82d6fffcdf42bddd
AgonoDavid <davidagono239@gmail.com>
2023-12-14 19:27:58 +0100

    ..

--- src/app/home/index.js
+++ src/app/home/index.js
@@ -1,7 +1,5 @@
 import React from "react";
-import { Box } from "@chakra-ui/react";
 import Layout from "../../layout";
-import Features from "../../components/features";
 
 const Home = () => {
   return <Layout></Layout>;
---
+++ src/components/partners/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import partner1 from "../../assets/Logo 1.png";
+import partner2 from "../../assets/Logo 2.png";
+import partner3 from "../../assets/Logo 3.png";
+import partner4 from "../../assets/Logo 4.png";
+import partner5 from "../../assets/Logo 5.png";
+import { Box, Img } from "@chakra-ui/react";
+
+const Partners = () => {
+  return (
+    <Box display={"flex"} justifyContent={"space-between"}>
+      <Box my={"auto"}>
+        <Img src={partner1} />
+      </Box>
+      <Box my={"auto"}>
+        <Img src={partner2} />
+      </Box>
+      <Box my={"auto"}>
+        <Img src={partner3} />
+      </Box>
+      <Box my={"auto"}>
+        <Img src={partner4} />
+      </Box>
+      <Box my={"auto"}>
+        <Img src={partner5} />
+      </Box>
+    </Box>
+  );
+};
+
+export default Partners;
