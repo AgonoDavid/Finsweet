4860675da5cb43ea629f97df5b7edceaa1fa826a
AgonoDavid <davidagono239@gmail.com>
2023-12-19 02:08:04 +0100

    ..

--- src/components/Navbar/Mobile.js
+++ src/components/Navbar/Mobile.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { Box, CloseButton } from "@chakra-ui/react";
+import { Box, CloseButton, Text } from "@chakra-ui/react";
+import CustomButton from "../Button";
+import { Link } from "react-router-dom";
 
 const Mobile = ({ show, onClose }) => {
   return (
@@ -28,8 +30,79 @@ const Mobile = ({ show, onClose }) => {
         padding="24px"
         boxShadow={show ? "-14px 0px 10px rgba(0, 0, 0, 0.1)" : "none"}
       >
-        <Box display="flex" justifyContent="flex-end" mb="30px">
+        <Box
+          display="flex"
+          justifyContent="flex-end"
+          mb="30px"
+          flexDir={"column"}
+        >
           <CloseButton onClick={onClose} />
+
+          <Box w={"100%"}>
+            <Box
+              display={"flex"}
+              flexDir={"column"}
+              alignItems={"center"}
+              gap={"40px"}
+              mt={"auto"}
+              mb={"auto"}
+              fontSize={"13px"}
+            >
+              {/*Home link*/}
+              <Link to="/">
+                <Text
+                  _hover={{ fontWeight: "bold", textDecoration: "underline" }}
+                >
+                  Home
+                </Text>
+              </Link>
+              {/* Product link*/}
+              <Link to="/product">
+                <Text
+                  _hover={{ fontWeight: "bold", textDecoration: "underline" }}
+                >
+                  Product
+                </Text>
+              </Link>
+              {/*pricing link*/}
+              <Link to="/pricing">
+                <Text
+                  _hover={{ fontWeight: "bold", textDecoration: "underline" }}
+                >
+                  Pricing
+                </Text>
+              </Link>
+              {/*about us link*/}
+              <Link to="/about">
+                <Text
+                  _hover={{ fontWeight: "bold", textDecoration: "underline" }}
+                >
+                  About
+                </Text>
+              </Link>
+              {/*blog link*/}
+              <Link to="/blog">
+                <Text
+                  _hover={{ fontWeight: "bold", textDecoration: "underline" }}
+                >
+                  Blog
+                </Text>
+              </Link>
+              {/*contact link*/}
+              <Link to="/contact">
+                <Text
+                  _hover={{ fontWeight: "bold", textDecoration: "underline" }}
+                >
+                  Contact
+                </Text>
+              </Link>
+              <Box>
+                <Link to="/pricing">
+                  <CustomButton>Free Trial</CustomButton>
+                </Link>
+              </Box>
+            </Box>
+          </Box>
         </Box>
       </Box>
     </Box>
